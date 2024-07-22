3f6a890d1f8d4cec4430bb3b787da482797b0aa6
realtobyfu <fuz.sh0710@gmail.com>
2024-07-22 15:18:08 -0400

    Initial Commit

---
+++ Ecrivez-local/Ecrivez_localApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ecrivez_localApp.swift
+//  Ecrivez-local
+//
+//  Created by Tobias Fu on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Ecrivez_localApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
