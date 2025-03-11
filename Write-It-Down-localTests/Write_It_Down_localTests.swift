70b2d909f58f9ef54992abe78199ae79197f3609
Tobias Fu <zfu04@tufts.edu>
2025-03-11 17:50:33 -0400

    Setting up unit tests

--- Write-It-Down-localTests/Write_It_Down_localTests.swift
+++ Write-It-Down-localTests/Write_It_Down_localTests.swift
@@ -6,11 +6,20 @@
 //
 
 import Testing
+import CoreData
+import Foundation
+import Ecrivez_local
 
+@Suite("Main tests")
 struct Write_It_Down_localTests {
 
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
+    
+//    @Test func test1() async throws {
+//        
+//        assert()
+//    }
 
 }
