a0241c8dfa5c3346cfbbcf4fd029cd05a449265a
Tobias Fu <zfu04@tufts.edu>
2025-03-11 14:27:01 -0400

    Created Swift Test Suite

---
+++ Write-It-Down-localTests/Write_It_Down_localTests.swift
@@ -0,0 +1,16 @@
+//
+//  Write_It_Down_localTests.swift
+//  Write-It-Down-localTests
+//
+//  Created by Tobias Fu on 3/11/25.
+//
+
+import Testing
+
+struct Write_It_Down_localTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
