159fb9894e1205e6be3ac0ad2941efed898d417d
Tobias Fu <zfu04@tufts.edu>
2025-04-09 02:13:31 -0400

    Centralize color mapping and categories using StyleManager

--- Ecrivez-local/Views/Screens/Feed/PublicNoteDetailView.swift
+++ Ecrivez-local/Views/Screens/Feed/PublicNoteDetailView.swift
@@ -503,20 +503,7 @@ struct PublicNoteDetailView: View {
     }
     
     private var backgroundColor: Color {
-        switch note.colorString {
-            case "green":   return .green
-            case "blue":    return .blue
-            case "yellow":  return .yellow
-            case "pink":    return .pink
-            case "brown":   return .brown
-            case "gray":    return .gray
-            case "red":     return .red
-            case "purple":  return .purple
-            case "orange":  return .orange
-            case "teal":    return .teal
-            case "indigo":  return .indigo
-            default:        return .black
-        }
+        return StyleManager.color(from: note.colorString)
     }
 }
 
